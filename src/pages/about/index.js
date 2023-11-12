dee3b73ae461797397bf3b19f3b8f259a0238e7c
Slardarz <WARpoks29>
2023-11-13 00:07:13 +0100

    img src changes

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -4,10 +4,10 @@ const About = () => {
     return (
         <div>
             <Box>
-            <img src='./pika.png' alt="Logo" width='800' />
+            <img src='pika.png' alt="Logo" width='800' />
             </Box>
             <Box>
-            <img src='./pikapika.png' alt="Logo" width='600' />
+            <img src='/pikapika.png' alt="Logo" width='600' />
             </Box>
             </div>
     );
